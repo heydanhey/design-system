4eeb5be7c73230e19ce3aa3c2b10c65ca70e1475
dhey <danhey@gmail.com>
2022-08-30 20:11:38 -0500

    test

--- packages/core/src/Button/index.tsx
+++ packages/core/src/Button/index.tsx
@@ -1,11 +1,31 @@
 import * as React from "react";
+import styled from 'styled-components';
+
+const StyledButton = styled
+  .button
+  .attrs(props => ({
+    type: props.type || 'button',
+  }))
+  `
+    background-color: transparent;
+    padding: 10px;
+    border-width: 5px;
+    border-color: blue;
+    border-radius: 5px;
+    transition: all 200ms ease-in-out;
+    aspect-ratio: 1 / 1;
+
+    :hover {
+      background-color: white;
+      opacity: 0.5;
+    }
+  `;
 
 export interface ButtonProps {
   children: React.ReactNode;
 }
-
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return <StyledButton {...props}>{props.children}</StyledButton>;
 }
 
 Button.displayName = "Button";
